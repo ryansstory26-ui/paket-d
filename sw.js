46da67e698657b75154f70ec65fc455f6ac197a5
Ryan's Story <ryansstory26@gmail.com>
2026-08-02 14:37:57 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,24 @@
-const CACHE_NAME = 'anca-kebab-v1';
+const CACHE_NAME = "anca-kebab-v1";
 const urlsToCache = [
-  './',
-  'index.html',
-  'manifest.json'
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
